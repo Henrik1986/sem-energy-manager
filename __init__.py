846edc640f637242369203b87479318ff42f8b7c
Henrik1986 <henrik.akerlund86@gmail.com>
2026-05-28 09:23:17 +0200

    Implement async setup and entry management functions

---
+++ __init__.py
@@ -0,0 +1,30 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+async def async_setup(hass: HomeAssistant, config: dict):
+
+```
+hass.data.setdefault(DOMAIN, {})
+
+return True
+```
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+
+```
+hass.data.setdefault(DOMAIN, {})
+
+hass.data[DOMAIN][entry.entry_id] = entry.data
+
+return True
+```
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+
+```
+hass.data[DOMAIN].pop(entry.entry_id)
+
+return True
+```
