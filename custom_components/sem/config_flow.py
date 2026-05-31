e5c98227a4b3db27d237a6b6731a2dbf6715e7ba
Henrik1986 <henrik.akerlund86@gmail.com>
2026-05-31 21:40:16 +0200

    Update config_flow.py

--- custom_components/sem/config_flow.py
+++ custom_components/sem/config_flow.py
@@ -11,12 +11,33 @@ class SEMConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
 
         if user_input is not None:
+            return await self.async_step_smhi()
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({}),
+        )
+
+    async def async_step_smhi(self, user_input=None):
+
+        smhi_loaded = self.hass.states.get("weather.smhi_home") is not None
+
+        smhi_status = (
+            "✔ SMHI konfigurerad"
+            if smhi_loaded
+            else "❌ SMHI saknas (skapa weather.smhi_home först)"
+        )
+
+        if user_input is not None:
             return self.async_create_entry(
                 title="SEM",
                 data={}
             )
 
         return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema({})
+            step_id="smhi",
+            data_schema=vol.Schema({}),
+            description_placeholders={
+                "status": smhi_status
+            }
         )
