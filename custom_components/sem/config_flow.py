52e47e6eae59f39913718f3d668227107fd2aeb5
Henrik1986 <henrik.akerlund86@gmail.com>
2026-05-31 21:19:12 +0200

    Add requirements check step in config flow

--- custom_components/sem/config_flow.py
+++ custom_components/sem/config_flow.py
@@ -10,13 +10,25 @@ class SEMConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
 
+        return await self.async_step_requirements_check()
+
+
+    async def async_step_requirements_check(self, user_input=None):
+
+        smhi_loaded = "sensor.smhi_weather" in self.hass.states
+
         if user_input is not None:
             return self.async_create_entry(
                 title="SEM",
                 data={}
             )
 
+        smhi_status = "✔ SMHI installerad" if smhi_loaded else "❌ SMHI saknas"
+
         return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema({})
+            step_id="requirements_check",
+            data_schema=vol.Schema({}),
+            description_placeholders={
+                "status": smhi_status
+            }
         )
