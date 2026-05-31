f92f3f1dfb3c7b5004ab49f5372c0cf8cc0af464
Henrik1986 <henrik.akerlund86@gmail.com>
2026-05-31 19:59:50 +0200

    Add configuration flow for SEM integration

---
+++ custom_components/sem/config_flow.py
@@ -0,0 +1,33 @@
+from homeassistant import config_entries
+import voluptuous as vol
+
+from .const import DOMAIN
+
+class SEMConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+
+```
+VERSION = 1
+
+async def async_step_user(self, user_input=None):
+
+    if user_input is not None:
+        return await self.async_step_requirements()
+
+    return self.async_show_form(
+        step_id="user",
+        data_schema=vol.Schema({})
+    )
+
+async def async_step_requirements(self, user_input=None):
+
+    if user_input is not None:
+        return self.async_create_entry(
+            title="SEM",
+            data={}
+        )
+
+    return self.async_show_form(
+        step_id="requirements",
+        data_schema=vol.Schema({})
+    )
+```
